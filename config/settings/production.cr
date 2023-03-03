7fe1565acef1f796624457d2372475f50802f1c2
Benjamin Guimberteau <b.guimberteau@mesvaccins.net>
2023-03-03 14:09:27 +0100

    security

--- config/settings/production.cr
+++ config/settings/production.cr
@@ -2,4 +2,5 @@ Marten.configure :production do |config|
   config.host = "0.0.0.0"
   config.port = 8000
   config.allowed_hosts = ["easydrills.guimberteau.com"]
+  config.use_x_forwarded_proto = true
 end
